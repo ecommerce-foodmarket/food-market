d7290c406538ee6ba3145bd310a3e138fd42b055
helenaponted <helenaponte@hotmail.es>
2023-09-05 09:31:35 +0100

    EC-20

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 // Route::resource('/user', UserController::class);
 
 // Route::get('/user', [UserController::class, 'index'])->name('allUsers');
